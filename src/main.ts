3fb0ff4ea783e1b37cbcbf752ee66085f0893e06
Lucas Ponce <ponce.ballesteros@gmail.com>
2020-11-22 00:36:03 +0100

    Warming up

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import * as github from "@actions/github";
 async function run() {
     const token = core.getInput("TOKEN", { required: true });
     const client = github.getOctokit(token);
-    const issues = client.issues.listLabelsForRepo();
+    const issues = client.issues.listForRepo({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+    })
     console.log(JSON.stringify(issues))
 }
 
