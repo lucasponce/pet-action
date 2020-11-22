706b8c3f3122c18d4abd76d4eeaa1550c0c570f4
Lucas Ponce <ponce.ballesteros@gmail.com>
2020-11-22 01:39:11 +0100

    Fixes

--- dist/index.js
+++ dist/index.js
@@ -1437,7 +1437,7 @@ function run() {
             octokit.issues.listForRepo({
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo,
-                labels: 'epic,epic%20subtask',
+                labels: 'epic,epic subtask',
             }).then(response => {
                 response.data.forEach(issue => {
                     console.log('issue #' + issue.number + ' - ' + issue.title);
